669fcab7977296c61eed17ff6af6b473d6feca0d
John Runyard <jrunyard3ft@gmail.com>
2021-05-07 22:09:57 -0500

    First Commit message.

--- Checklist App/ContentView.swift
+++ Checklist App/ContentView.swift
@@ -11,7 +11,8 @@ struct ContentView: View {
     var body: some View {
         Text("Hello, world!")
             .padding()
-    }
+        Text("Hello, world!")
+            .padding()    }
 }
 
 struct ContentView_Previews: PreviewProvider {
