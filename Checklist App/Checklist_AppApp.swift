2521ba60d64aff95675586880f6223b0eb9cea61
John Runyard <jrunyard3ft@gmail.com>
2021-05-07 22:05:37 -0500

    Initial Commit

---
+++ Checklist App/Checklist_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Checklist_AppApp.swift
+//  Checklist App
+//
+//  Created by Runyard, John on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct Checklist_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
